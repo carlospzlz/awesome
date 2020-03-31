56dbd25cb5b5a8db2e32fa0bebf8faf53bf71c0f
Carlos Perez-Lopez <carlospzlz@gmail.com>
2020-03-31 23:00:38 +0100

    Now showing the battery widget with color codes!

--- widgets/ramanujan/battery.lua
+++ widgets/ramanujan/battery.lua
@@ -10,12 +10,28 @@ local battery_text = wibox.widget{
 local battery_widget = wibox.widget.background()
 battery_widget:set_widget(battery_text)
 
--- Monitor battery
+-- Monitor battery capacity
 watch('fish -c "cat /sys/class/power_supply/BAT0/capacity"', 10,
-      function(widget, stdout, stderr, exitreason, exitcode)
-	      local battery = stdout:gsub("%s+", "")
-          local text = string.format(" %s%% ", battery)
+      function(_, stdout, stderr, exitreason, exitcode)
+	      local capacity = stdout:gsub("%s+", "")
+          local text = string.format(" %s%% ", capacity)
 	      battery_text:set_text(text)
+		  if (tonumber(capacity) < 10) then
+		      battery_widget:set_fg("#ff0000")
+		  end
+	  end,
+	  battery_widget
+)
+
+-- Monitor battery status
+watch('fish -c "cat /sys/class/power_supply/BAT0/status"', 10,
+      function(_, stdout, stderr, exitreason, exitcode)
+	      local status = stdout:gsub("%s+", "")
+		  if (status == "Charging") then
+		      battery_widget:set_fg("#00FF00")
+		  else
+		      battery_widget:set_fg("#BBBBBB")
+		  end
 	  end,
 	  battery_widget
 )
