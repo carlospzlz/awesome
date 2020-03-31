5a212be10ef65e8fda27a1836292a5eb44da6577
Carlos Perez-Lopez <carlospzlz@gmail.com>
2020-03-31 23:33:11 +0100

    Temperature widget now uses color codes!

--- widgets/eratosthenes/temperature.lua
+++ widgets/eratosthenes/temperature.lua
@@ -12,12 +12,20 @@ temperature_widget:set_widget(temperature_text)
 
 -- Monitor temperature
 watch('fish -c "string sub -s 18 -l 2 (sensors | grep Tdie)"', 5,
-      function(widget, stdout, stderr, exitreason, exitcode)
-	      local temp = stdout:gsub("%s+", "")
-          local text = string.format(" %s°C ", temp)
-	      temperature_text:set_text(text)
-	  end,
-	  temperature_widget
+      function(_, stdout, stderr, exitreason, exitcode)
+          local temp_str = stdout:gsub("%s+", "")
+          local text = string.format(" %s°C ", temp_str)
+          temperature_text:set_text(text)
+          local temp_num = tonumber(temp_str)
+          if (temp_num < 60) then
+              temperature_widget:set_fg("#AAAAAA")
+          elseif (temp_num < 70) then
+              temperature_widget:set_fg("#FFA500")
+          else
+              temperature_widget:set_fg("#FF0000")
+          end
+      end,
+      temperature_widget
 )
 
 return temperature_widget
